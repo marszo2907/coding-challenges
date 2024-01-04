f184c3a34c213d90f09942c06f974f29d483e941
Marcin Szostak <m.szostak2907@icloud.com>
2024-01-04 07:59:04 +0100

    1.1 Check Permutations hash table

--- arrays_and_strings/CheckPermutation.java
+++ arrays_and_strings/CheckPermutation.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Given two strings, write a method to decide if oe is a permutation of the other.
@@ -18,6 +20,7 @@ public class CheckPermutation {
         char[] string1 = args[1].toCharArray();
 
         System.out.printf("Using a brute force algorithm: %s%n", sovleWithBruteForce(string0, string1) ? "true" : "false");
+        System.out.printf("Using a hash table: %s%n", solveWithHashTable(string0, string1) ? "true" : "false");
     }
 
     private static void calculatePermutations(List<char[]> permutations, char[] string) {
@@ -54,6 +57,40 @@ public class CheckPermutation {
             }
         }
         return false;
+    }
+
+    /**
+     * This solution maps each character of the string0 to its count, iterates through each character of the string1 and decreases its mapped count by 1.
+     * Strings are equal if and only if each mapped character has a count of 0.
+     * Time complexity: O(s)
+     * Space complexity: O(s)
+     */
+    private static boolean solveWithHashTable(char[] string0, char[] string1) {
+        if (string0.length != string1.length) {
+            return false;
+        }
+
+        Map<Character, Integer> characterCount = new HashMap<>();
+        for (var character : string0) {
+            int currentCount = characterCount.getOrDefault(character, 0);
+            characterCount.put(character, currentCount + 1);
+        }
+
+        for (var character : string1) {
+            int currentCount = characterCount.getOrDefault(character, 0);
+            if (0 >= currentCount) {
+                return false;
+            }
+            characterCount.put(character, currentCount - 1);
+        }
+
+        for (var key : characterCount.keySet()) {
+            int count = characterCount.get(key);
+            if (0 != count) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static void swap(char[] string, int index1, int index2) {
