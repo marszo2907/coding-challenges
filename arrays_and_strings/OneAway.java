0de456556bff14f8da2b1853357b36dadb970011
Marcin Szostak <m.szostak2907@icloud.com>
2024-01-05 21:32:23 +0100

    1.5 One Away removed redundant imports

--- arrays_and_strings/OneAway.java
+++ arrays_and_strings/OneAway.java
@@ -1,6 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * There are three types of edits that can be performed on strings: insert a character, remove a character or replace a character.
  * Given two strings, write a function to check if they are one or zero edits away.
